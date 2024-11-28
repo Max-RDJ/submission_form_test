35ab02509357b3b18b2d4cc5d71f5dcc88bb0ce2
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-28 16:11:22 +0000

    Submission confirmation functionality

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,8 @@
 function confirmSubmit() {
-    return confirm('Are you sure you want to submit?')
+    if (confirm('Are you sure you want to submit?') == true) {
+        document.getElementById("submissionConfirmed").style.display = "block";
+        return true;
+    } else {
+        return false;
+    }
 }
