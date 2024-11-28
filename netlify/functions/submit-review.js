63f5ec792ce512683d941260d834bbe00e3bddc4
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-28 11:45:44 +0000

    Added files for Netlify backend

---
+++ netlify/functions/submit-review.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+
+exports.handler = async (event) => {
+  const data = JSON.parse(event.body);
+
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "review",
+    port: "3307"
+  });
+
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "INSERT INTO name_and_review (firstName, lastName, review) VALUES (?, ?, ?)",
+      [data.firstName, data.lastName, data.review],
+      (error, results) => {
+        if (error) {
+          reject({ statusCode: 500, body: JSON.stringify(error) });
+        } else {
+          resolve({ statusCode: 200, body: "Review submitted successfully" });
+        }
+        connection.end();
+      }
+    );
+  });
+};
+
+
+// Add spam filter?
