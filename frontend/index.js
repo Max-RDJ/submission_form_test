2b6fab29c0e0f7d97159456c3116af53b1fe9f17
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-28 16:42:49 +0000

    "Thank you" feedback message

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,8 @@
 function confirmSubmit() {
     if (confirm('Are you sure you want to submit?') == true) {
+        console.log("Form submitted.")
         document.getElementById("submissionConfirmed").style.display = "block";
+        document.getElementById()
         return true;
     } else {
         return false;
