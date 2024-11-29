533b1dd63ae021e1eae59cd3aa5b8065293ac2b7
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-29 13:47:17 +0000

    Debugging
    Empty entries are being sent to db

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,9 @@ function confirmSubmit() {
     if (confirm('Are you sure you want to submit?') == true) {
         console.log("Form submitted.")
         document.getElementById("submissionConfirmed").style.display = "block";
-        document.getElementById("firstName").value = "";
-        document.getElementById("lastName").value = "";
-        document.getElementById("review").value = "";
+        // document.getElementById("firstName").value = "";
+        // document.getElementById("lastName").value = "";
+        // document.getElementById("review").value = "";
         return true;
     } else {
         return false;
