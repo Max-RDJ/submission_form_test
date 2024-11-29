b05da788630d29f29098f63cfc52af29d3a8c9f0
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-29 14:05:48 +0000

    Should not be clearing inputs

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,12 @@ function confirmSubmit() {
     if (confirm('Are you sure you want to submit?') == true) {
         console.log("Form submitted.")
         document.getElementById("submissionConfirmed").style.display = "block";
+
+        // SHOULD NOT BE CLEARING INPUTS
         // document.getElementById("firstName").value = "";
         // document.getElementById("lastName").value = "";
         // document.getElementById("review").value = "";
+        
         return true;
     } else {
         return false;
