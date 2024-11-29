25e78cbed06bfae647ba0a3bdc070cd90e48666b
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-29 09:49:26 +0000

    Submit clears input boxes

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,9 @@ function confirmSubmit() {
     if (confirm('Are you sure you want to submit?') == true) {
         console.log("Form submitted.")
         document.getElementById("submissionConfirmed").style.display = "block";
-        document.getElementById()
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        document.getElementById("review").value = "";
         return true;
     } else {
         return false;
